03f364b335b61f36cdf74b72d3e3da06c3cb88e5
Sedat Ay <sedatay30@gmail.com>
2024-03-25 10:12:13 +0300

    PatikaStore

---
+++ src/Brand.java
@@ -0,0 +1,19 @@
+
+public class Brand {
+    private int id;
+    private String brandName;
+
+    public Brand(int id, String brandName) {
+        this.id = id;
+        this.brandName = brandName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+}
